c09480adb859f3ce6b86884f7db0457cb9f0d7ba
fenol64 <fenol64.dev@gmail.com>
2023-11-17 15:50:48 -0300

    gnl to libft

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,37 @@
+#include "get_next_line_bonus.h"
+
+char	*read_file(int fd)
+{
+	int			read_return;
+	char		*line;
+	static char	*res[MAX_FD] = {NULL};
+
+	line = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	read_return = 1;
+	while (read_return > 0 && !find_current_line(res[fd]))
+	{
+		read_return = read(fd, line, BUFFER_SIZE);
+		if (read_return < 0)
+		{
+			free(line);
+			return (NULL);
+		}
+		line[read_return] = '\0';
+		res[fd] = ft_free_first_strjoin(res[fd], line);
+	}
+	free(line);
+	if (!res[fd])
+		return (NULL);
+	line = return_current_line(res[fd]);
+	res[fd] = return_after_sep(res[fd]);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	return (read_file(fd));
+}
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,112 @@
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	find_current_line(char *str)
+{
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char	*return_current_line(char *res)
+{
+	int		i;
+	int		check;
+	int		size;
+	char	*line;
+
+	size = 0;
+	check = 0;
+	while (res[size] && res[size] != '\n')
+		size++;
+	if (res[size] == '\n')
+		check++;
+	line = malloc((size + check + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		line[i] = res[i];
+		i++;
+	}
+	if (res[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*return_after_sep(char *res)
+{
+	char	*new_res;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (res[size] && res[size] != '\n')
+		size++;
+	if (!res[size])
+	{
+		free(res);
+		return (NULL);
+	}
+	size++;
+	new_res = malloc((ft_strlen(res) - size + 1) * sizeof(char));
+	if (!new_res)
+		return (NULL);
+	i = 0;
+	while (res[size + i])
+	{
+		new_res[i] = res[size + i];
+		i++;
+	}
+	new_res[i] = '\0';
+	free(res);
+	return (new_res);
+}
+
+char	*ft_free_first_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	if (s1)
+	{
+		while (s1[i])
+		{
+			str[i] = s1[i];
+			i++;
+		}
+		free(s1);
+	}
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = '\0';
+	if (!*str)
+	{
+		free(str);
+		return (NULL);
+	}
+	return (str);
+}
